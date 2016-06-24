101b76cdb0f96e3be19f95be57d4d088e84f6e53
lidox <schaefa@mail.ru>
2016-06-24 11:20:28 +0200

    add prototype goGame color change

--- ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/games/GoGameView.java
+++ ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/games/GoGameView.java
@@ -8,7 +8,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.artursworld.reactiontest.R;
 import com.artursworld.reactiontest.controller.helper.GameStatus;
@@ -20,14 +22,14 @@ import java.util.Random;
 public class GoGameView extends AppCompatActivity {
 
     private Activity activity;
-    private GameStatus gameStatus;
+    private GameStatus statusOfGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_game);
         activity = this;
-        gameStatus = GameStatus.WAITING;
+        statusOfGame = GameStatus.WAITING;
 
         UtilsRG.info("received message="+getIntentMessage(UserManagement.EXTRA_MESSAGE));
 
@@ -77,7 +79,24 @@ public class GoGameView extends AppCompatActivity {
     private void onChangeStatusToClick() {
         UtilsRG.info("set different color");
         setBackGroundColor(activity, R.color.goGameGreen);
-        gameStatus = GameStatus.CLICK;
+        statusOfGame = GameStatus.CLICK;
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            String text = "x = " + event.getX() + ", y = " + event.getY();
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+            if(statusOfGame == GameStatus.CLICK){
+                UtilsRG.info("right!");
+                setBackGroundColor(this, R.color.goGameBlue);
+                statusOfGame = GameStatus.WAITING;
+                waitAndChangeStatusToClick(2000,4000);
+            }
+            else{
+                UtilsRG.info("to early");
+            }
+        }
+        return super.onTouchEvent(event);
+    }
 }
