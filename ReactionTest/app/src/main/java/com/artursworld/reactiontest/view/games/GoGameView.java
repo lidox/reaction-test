04eb6c0ca5bb40cf147fbbf10d369e1992b362cf
lidox <schaefa@mail.ru>
2016-06-28 20:45:59 +0200

    add countdown to gogame

--- ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/games/GoGameView.java
+++ ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/games/GoGameView.java
@@ -3,6 +3,7 @@ package com.artursworld.reactiontest.view.games;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.artursworld.reactiontest.R;
@@ -23,6 +25,10 @@ public class GoGameView extends AppCompatActivity {
 
     private Activity activity;
     private GameStatus statusOfGame;
+    private TextView countDownText;
+    private int countDown_sec = 4;
+    private int minWaitTimeAtStartUp_sec = 2;
+    private int maxWaitTimeAtStartUp_sec = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +41,28 @@ public class GoGameView extends AppCompatActivity {
 
         hideActionBar(getSupportActionBar());
         setBackGroundColor(activity, R.color.goGameBlue);
-        waitAndChangeStatusToClick(2000, 5000);
+        runCountDown();
+    }
+
+    private void runCountDown() {
+        countDownText = (TextView) findViewById(R.id.gogamecountdown);
+        new CountDownTimer(countDown_sec * 1000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                if(countDownText != null)
+                    countDownText.setText("" + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                onCountDownFinish();
+            }
+        }.start();
+    }
+
+    private void onCountDownFinish() {
+        waitAndChangeStatusToClick(minWaitTimeAtStartUp_sec * 1000, maxWaitTimeAtStartUp_sec * 1000);
+        if(countDownText != null)
+            countDownText.setText(R.string.attention);
     }
 
     private String getIntentMessage(String messageKey){
@@ -78,6 +105,8 @@ public class GoGameView extends AppCompatActivity {
 
     private void onChangeStatusToClick() {
         UtilsRG.info("set different color");
+        if(countDownText != null)
+            countDownText.setText(R.string.click);
         setBackGroundColor(activity, R.color.goGameGreen);
         statusOfGame = GameStatus.CLICK;
     }
@@ -85,18 +114,22 @@ public class GoGameView extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event){
         if(event.getAction() == MotionEvent.ACTION_DOWN){
-            String text = "x = " + event.getX() + ", y = " + event.getY();
-            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+            //String text = "x = " + event.getX() + ", y = " + event.getY();
+            //Toast.makeText(this, text, Toast.LENGTH_LONG).show();
             if(statusOfGame == GameStatus.CLICK){
-                UtilsRG.info("right!");
-                setBackGroundColor(this, R.color.goGameBlue);
-                statusOfGame = GameStatus.WAITING;
-                waitAndChangeStatusToClick(2000,4000);
+                onCorrectTouch();
             }
             else{
                 UtilsRG.info("to early");
             }
         }
         return super.onTouchEvent(event);
+    }
+
+    private void onCorrectTouch() {
+        UtilsRG.info("User touched at correct moment");
+        setBackGroundColor(this, R.color.goGameBlue);
+        statusOfGame = GameStatus.WAITING;
+        runCountDown();
     }
 }
