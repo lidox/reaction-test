6746f4dd1f6c17e5fa821423894ea52d842a6df0
lidox <schaefa@mail.ru>
2016-06-30 16:27:51 +0200

    add basic medical user view

--- ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/UserManagement.java
+++ ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/UserManagement.java
@@ -9,6 +9,7 @@ import com.artursworld.reactiontest.R;
 import com.artursworld.reactiontest.model.persistence.manager.MedicalUserManager;
 import com.artursworld.reactiontest.controller.util.UtilsRG;
 import com.artursworld.reactiontest.view.games.GoGameView;
+import com.artursworld.reactiontest.view.user.AddMedicalUser;
 
 public class UserManagement extends AppCompatActivity {
 
@@ -41,6 +42,11 @@ public class UserManagement extends AppCompatActivity {
         //EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = "super secret message from first view";//editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void startCreateMedicalUser(View view){
+        Intent intent = new Intent(this, AddMedicalUser.class);
         startActivity(intent);
     }
 }
---
+++ ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/user/AddMedicalUser.java
@@ -0,0 +1,15 @@
+package com.artursworld.reactiontest.view.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.artursworld.reactiontest.R;
+
+public class AddMedicalUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_medical_user);
+    }
+}
