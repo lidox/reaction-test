f509148ec80d910651252eda7467eb5bdb5d313d
lidox <schaefa@mail.ru>
2016-07-15 11:43:58 +0200

    state before adding tabs

---
+++ ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/user/OperationInformation.java
@@ -0,0 +1,55 @@
+package com.artursworld.reactiontest.view.user;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.artursworld.reactiontest.R;
+
+public class OperationInformation extends Fragment {
+
+
+    public OperationInformation() {
+        // Required empty public constructor
+    }
+
+    public static OperationInformation newInstance(String param1, String param2) {
+        OperationInformation fragment = new OperationInformation();
+        Bundle args = new Bundle();
+        //args.putString(ARG_PARAM1, param1);
+        //args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_operation_information, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+
+}
