3b046fa09b4ea6904d4fa810ffc9299a26bafda1
lidox <schaefa@mail.ru>
2016-09-15 15:31:33 +0200

    refactor code

--- ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/LauncherView.java
+++ ReactionTest/app/src/main/java/com/artursworld/reactiontest/view/LauncherView.java
@@ -1,6 +1,7 @@
 package com.artursworld.reactiontest.view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.artursworld.reactiontest.R;
 import com.artursworld.reactiontest.controller.util.UtilsRG;
@@ -32,7 +34,6 @@ public class LauncherView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_launcher_view);
     }
 
